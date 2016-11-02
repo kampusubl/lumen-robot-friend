0f75a9d966c5bff2957762704fdadd1365032a56
Ari Kurniawan Saputra <arikurniawans@users.noreply.github.com>
2016-11-02 18:18:42 +0700

    Add files via upload

---
+++ FPSNas/FPSNas/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace FPSNas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            var fileName = Path.GetFileName(@"C:\jajal.jpg");
+            var request = (FtpWebRequest)WebRequest.Create("ftp://167.205.66.69/coba/" + fileName);
+
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential("admin", "lumen");
+            request.UsePassive = true;
+            request.UseBinary = true;
+            request.KeepAlive = false;
+
+            using (var fileStream = File.OpenRead(@"C:\jajal.jpg"))
+            {
+                using (var requestStream = request.GetRequestStream())
+                {
+                    fileStream.CopyTo(requestStream);
+                    requestStream.Close();
+                }
+            }
+
+            var response = (FtpWebResponse)request.GetResponse();
+            Console.WriteLine("Upload done: {0}", response.StatusDescription);
+            response.Close();
+        }
+    }
+}
---
+++ FPSNas/FPSNas/koneksi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace FPSNas
+{
+    class koneksi
+    {
+        public static void uploadFTP(string srcFilePath, string ftpUserName, string ftpPassword, string targetFilePath)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(srcFilePath);
+            request.Credentials = new NetworkCredential(
+            ftpUserName, ftpPassword);
+            request.Proxy = null;
+            request.UseBinary = false;
+            request.UsePassive = true;
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            StreamReader sourceStream = new StreamReader(targetFilePath);
+            byte[] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+            sourceStream.Close();
+            request.ContentLength = fileContents.Length;
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(fileContents, 0, fileContents.Length);
+            requestStream.Close();
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            response.Close();
+        }
+    }
+
+   
+}
